f5209d42a6b8ff91a00625a413a1155d9b6f9cfb
scanossjs <juan.m.salamanca@scanoss.com>
2021-01-27 09:48:47 +0100

    1.1.9 Fix memory leak

--- inc/scanner.h
+++ inc/scanner.h
@@ -28,7 +28,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define VERSION "1.1.8"
+#define VERSION "1.1.9"
 #define MAX_HEADER_LEN 1024 * 1024 * 1024 * 10
 #define MAX_FILE_SIZE (1024 * 1024 * 4)
 #define MIN_FILE_SIZE 128
